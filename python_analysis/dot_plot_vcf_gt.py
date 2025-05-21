0947bc06df81131be901902c948aefb45ead839b
Miguel Brown <miguel.a.brown@gmail.com>
2025-05-21 19:28:29 +0000

    :wrench: fixed various bugs

--- python_analysis/dot_plot_vcf_gt.py
+++ python_analysis/dot_plot_vcf_gt.py
@@ -2,11 +2,9 @@
 """Plot SNP genotype and VAF data using ggplot into dotplot."""
 
 import argparse
-import pdb
-from turtle import color
 
 import pandas as pd
-from ggplot import ggplot, aes, geom_point, geom_hline, labs, theme, element_text, scale_x_discrete
+from plotnine import aes, element_text, geom_hline, geom_point, ggplot, labs, theme
 
 
 def main():
@@ -15,26 +13,37 @@ def main():
     )
     parser.add_argument("-i", "--input", type=str, required=True, help="Input file with SNP data")
     parser.add_argument("-o", "--output", type=str, required=True, help="Output file for the plot")
+    parser.add_argument(
+        "-t",
+        "--title",
+        type=str,
+        required=False,
+        help="Title of the plot",
+        default="SNP Genotype and VAF Plot",
+    )
     args = parser.parse_args()
-
     # Read the input data
     snp_gt_vaf_data: pd.DataFrame = pd.read_csv(args.input, delimiter="\t")
-    snp_gt_vaf_data['rs_id'] = snp_gt_vaf_data['rs_id'].astype(str)
-    snp_gt_vaf_data['sample_genotype'] = snp_gt_vaf_data['sample_genotype'].astype(str)
+    snp_gt_vaf_data = snp_gt_vaf_data.convert_dtypes()
     # Create the plot
     p = (
-        ggplot(snp_gt_vaf_data, aes(x="rs_id", y="sample_vaf", colour="sample_genotype"))
-        + geom_point()
-        # + geom_hline(yintercept=[0, 0.33, 0.66, 1.0], linetype="dashed")
+        ggplot(snp_gt_vaf_data, aes("rs_id", "sample_vaf"))
+        + geom_point(aes(color="sample_genotype"), alpha=0.5, size=1)
         + labs(
-            title="chr21 Common SNPs Genotyped VCF",
-            x="rs ID",
+            title=args.title,
+            x="SNP ID",
             y="VAF",
         )
-        + theme(axis_text_x=element_text(angle=90))
+        + theme(
+            axis_text_x=element_text(angle=90),
+            legend_title=element_text(text="Genotype"),
+            legend_position="bottom",
+            figure_size=(7, 8)
+        )
+        + geom_hline(yintercept=[0, 0.33, 0.66, 1.0], linetype="dashed", color="grey", alpha=0.5)
     )
     # Save the plot
-    p.save(args.output)
+    p.save(args.output, dpi=300)
 
 
 if __name__ == "__main__":
--- python_analysis/sb_collate_snp_gt_vaf.py
+++ python_analysis/sb_collate_snp_gt_vaf.py
@@ -42,7 +42,7 @@ def get_dbsnp_coords(
     return dbsnp_coord
 
 
-def parse_vcfs(dbsnp: dict[str, list], vcf_fname: str, sample_id: str) -> list[str]:
+def parse_vcfs(dbsnp: dict[str, list], vcf_fname: str, print_sample_id: str) -> list[str]:
     """Parse VCF files from a sbfs mount and collate the SNP genotype and VAF data.
 
     Args:
@@ -53,50 +53,112 @@ def parse_vcfs(dbsnp: dict[str, list], vcf_fname: str, sample_id: str) -> list[s
     """
     print_list: list[str] = []
     with VariantFile(vcf_fname, threads=4) as vcf_file:
-        print(f"Processing {vcf_fname} for {sample_id}", file=sys.stderr)
+        print(f"Processing {vcf_fname} for {print_sample_id}", file=sys.stderr)
         try:
             for rs_id in dbsnp:
                 # format location as SAM region string as it's 1-based
                 sam_region = f"{dbsnp[rs_id][0]}:{dbsnp[rs_id][1]}-{dbsnp[rs_id][1]}"
-                record: VariantRecord = vcf_file.fetch(
-                    region=sam_region
-                ).__next__()
-                # We want it to either be a call in a ref block, or an already annotated rs_id
-                if (
-                    record.id is None
-                    or record.pos != dbsnp[rs_id][1]
-                    or (record.alts is not None and record.alts[0] in dbsnp[rs_id][3])
-                ):
-                    # Get the genotype
-                    gt: str = "/".join(map(str, record.samples[sample_id]["GT"]))
-                    # Get the VAF
-                    if "AD" in record.samples[sample_id] and record.samples[sample_id]["DP"] > 0:
-                        vaf: float = (
-                            record.samples[sample_id]["AD"][1] / record.samples[sample_id]["DP"]
+                for record in vcf_file.fetch(region=sam_region):
+                    sample_id = record.samples.keys()[0]
+                    # We want it to either be a call in a ref block, or an already annotated rs_id
+                    if (
+                        record.alts[0] == "<NON_REF>"
+                        or (record.id is not None and record.id == rs_id)
+                        or (
+                            record.pos == dbsnp[rs_id][1]
+                            and (record.alts is not None and record.alts[0] in dbsnp[rs_id][3])
+                        )
+                    ):
+                        # Get the genotype
+                        gt: str = "/".join(map(str, record.samples[sample_id]["GT"]))
+                        # Get the VAF
+                        if (
+                            "AD" in record.samples[sample_id]
+                            and record.samples[sample_id]["DP"] > 0
+                        ):
+                            vaf: float = (
+                                record.samples[sample_id]["AD"][1] / record.samples[sample_id]["DP"]
+                            )
+                        else:
+                            vaf = 0.0
+
+                        # Print the results
+                        print_list.append(
+                            f"{print_sample_id}\t{rs_id}\t{dbsnp[rs_id][0]}\t{dbsnp[rs_id][1]}\t{gt}\t{vaf}"
                         )
                     else:
-                        vaf = 0
-
-                    # Print the results
-                    print_list.append(
-                        f"{sample_id}\t{rs_id}\t{dbsnp[rs_id][0]}\t{dbsnp[rs_id][1]}\t{gt}\t{vaf}"
-                    )
-                else:
-                    print(
-                        f"Skipping {rs_id} {record.pos} in {vcf_fname}",
-                        file=sys.stderr,
-                    )
+                        print(
+                            f"Skipping {rs_id} {record.pos} in {vcf_fname}",
+                            file=sys.stderr,
+                        )
         except Exception as e:
             print(f"Error processing {vcf_fname}: {e}", file=sys.stderr)
             sys.exit(1)
         return print_list
 
 
+def parse_joint_vcf(dbsnp: dict[str, list], vcf_fname: str, out_file: IO):
+    """Parse a joint VCF file and collate the SNP genotype and VAF data.
+
+    Args:
+        dbsnp (dict[str, list]): Dictionary with rsID as key and a list of [chrom, pos] as value.
+        vcf_fname (str): Path to the VCF file.
+        out_file (IO): Output file handle to write the results to.
+
+    """
+    with VariantFile(vcf_fname, threads=8) as vcf_file:
+        print(f"Processing joint calls {vcf_fname}", file=sys.stderr)
+        for rs_id in dbsnp:
+            print(f"Processing {rs_id}", file=sys.stderr)
+            # format location as SAM region string as it's 1-based
+            sam_region = f"{dbsnp[rs_id][0]}:{dbsnp[rs_id][1]}-{dbsnp[rs_id][1]}"
+            try:
+                record: VariantRecord = vcf_file.fetch(region=sam_region).__next__()
+                for sample_id in record.samples:
+                    # We want it to either be a call in a ref block, or an already annotated rs_id
+                    if (record.id is not None and record.id == rs_id) or (
+                        record.pos == dbsnp[rs_id][1]
+                        and (record.alts is not None and record.alts[0] in dbsnp[rs_id][3])
+                    ):
+                        # Get the genotype
+                        gt: str = "/".join(map(str, record.samples[sample_id]["GT"]))
+                        # Get the VAF
+                        if (
+                            "AD" in record.samples[sample_id]
+                            and record.samples[sample_id]["DP"] > 0
+                        ):
+                            vaf: float = (
+                                record.samples[sample_id]["AD"][1] / record.samples[sample_id]["DP"]
+                            )
+                        else:
+                            vaf = 0.0
+
+                        # Print the results
+                        print(
+                            f"{sample_id}\t{rs_id}\t{dbsnp[rs_id][0]}\t{dbsnp[rs_id][1]}\t{gt}\t{vaf}",
+                            file=out_file,
+                        )
+                    else:
+                        print(
+                            f"Skipping {rs_id} {record.pos} in {vcf_fname}",
+                            file=sys.stderr,
+                        )
+
+            except Exception as e:
+                print(
+                    f"{e}. Skipping {rs_id} {dbsnp[rs_id][0]}:{dbsnp[rs_id][1]}, no calls found",
+                    file=sys.stderr,
+                )
+                # Need to reopen VCF file iterator for the next rs_id
+                # This is a workaround for the StopIteration error
+                vcf_file.reset()
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=(
             "Collate SNP genotype and VAF data from multiple files into a single file."
-            "Prints to stdout"
+            "Use manifest ot single joint VCF"
         ),
     )
     parser.add_argument("-d", "--dbsnp", action="store", dest="dbsnp", help="path to dbsnp file")
@@ -113,6 +175,13 @@ def main():
         action="store",
         dest="manifest",
         help="CAVATICA VCF manifest",
+    )
+    parser.add_argument(
+        "-j",
+        "--joint_vcf",
+        action="store",
+        dest="joint_vcf",
+        help="Joint VCF file instead of manifest",
     )
     parser.add_argument(
         "-r",
@@ -152,34 +221,42 @@ def main():
         with open(args.rs_id_list) as rs_id_file:
             rs_id_list = rs_id_file.read().splitlines()
     dbsnp: dict[str, list] = get_dbsnp_coords(args.dbsnp, rs_id_list, args.contig)
-
-    with open(args.manifest) as manifest, open(args.out_file, "w") as out_file:
-        # print header
+    with open(args.out_file, "w") as out_file:
         print("sample_id\trs_id\tchrom\tpos\tsample_genotype\tsample_vaf", file=out_file)
-        manifest_file = csv.reader(manifest, delimiter="\t")
-        head: list[str] = next(manifest_file)
-        name_idx: int = head.index("name")
-        sample_idx: int = head.index(args.field)
-        with ProcessPoolExecutor(max_workers=4) as executor:
-            tasks = [
-                executor.submit(
-                    parse_vcfs, dbsnp, f"{args.sb_mount}/{line[name_idx]}", line[sample_idx]
-                )
-                for line in manifest_file
-                if not line[name_idx].endswith("tbi")
-            ]
-            try:
-                for task in as_completed(tasks):
-                    print(*task.result(), sep="\n", file=out_file)
-            except KeyboardInterrupt:
-                print("Keyboard interrupt, exiting", file=sys.stderr)
-                executor.shutdown(wait=False, cancel_futures=True)
-                sys.exit(1)
-            except TimeoutError:
-                print("Timeout occurred during shutdown.", file=sys.stderr)
-            finally:
-                out_file.close()
+        if args.manifest is not None:
+            with open(args.manifest) as manifest:
+                # print header
+                manifest_file = csv.reader(manifest, delimiter="\t")
+                head: list[str] = next(manifest_file)
+                name_idx: int = head.index("name")
+                sample_idx: int = head.index(args.field)
+                with ProcessPoolExecutor(max_workers=4) as executor:
+                    tasks = [
+                        executor.submit(
+                            parse_vcfs, dbsnp, f"{args.sb_mount}/{line[name_idx]}", line[sample_idx]
+                        )
+                        for line in manifest_file
+                        if not line[name_idx].endswith("tbi")
+                    ]
+                    try:
+                        for task in as_completed(tasks):
+                            print(*task.result(), sep="\n", file=out_file)
+                    except KeyboardInterrupt:
+                        print("Keyboard interrupt, exiting", file=sys.stderr)
+                        executor.shutdown(wait=False, cancel_futures=True)
+                        sys.exit(1)
+                    except TimeoutError:
+                        print("Timeout occurred during shutdown.", file=sys.stderr)
+                    finally:
+                        out_file.close()
+        elif args.joint_vcf is not None:
+            parse_joint_vcf(dbsnp, args.joint_vcf, out_file)
+        else:
+            print("No manifest or joint VCF file provided, exiting", file=sys.stderr)
+            sys.exit(1)
+
     print("Finished processing all files", file=sys.stderr)
+
 
 if __name__ == "__main__":
     main()
